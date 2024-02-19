f7063b80ea93ebc660ba608df01b1187b60a53da
Movlan <movlan.mustafayev@gmail.com>
2024-02-19 17:11:02 +0400

    kontakt

---
+++ app.js
@@ -0,0 +1,6 @@
+const langBtn = document.getElementById("lang")
+const lang2 = document.getElementById("lang2")
+
+langBtn.addEventListener('click',function(){
+    lang2.classList.toggle('show')
+})
